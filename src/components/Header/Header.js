77b882d373d2f6258c57e1198fb807dc353a1235
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-25 14:31:43 -0700

    creating contact component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,17 @@ function Header() {
   const location = useLocation();
 
   return (
-    <div class="card text-center"style={{background: "#659DBD"}}>
+    <div className="card text-center"style={{background: "#659DBD"}}>
       <h1 className="display-5 fw-bold">Sergio Gomez's Portfolio</h1>
-      <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs nav-fill">
-          <li class="nav-item">
+      <div className="card-header">
+        <ul className="nav nav-tabs card-header-tabs nav-fill">
+          <li className="nav-item">
           <Link to="/" style={{color: "black"}}className={location.pathname === "/" ? "nav-link active" : "nav-link"}>About</Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="projects" style={{color:"black"}}className={location.pathname === "/projects" ? "nav-link active" : "nav-link"}>Projects</Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
           <Link to="contact" style={{color:"black"}}className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
           </li>
         </ul>
