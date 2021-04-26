4f1d93c14ea2a9aa7e667f5ccee89d68216e3edb
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-25 17:15:05 -0700

    mapping thru projects

--- src/components/ProjectInfo/Project.js
+++ src/components/ProjectInfo/Project.js
@@ -3,8 +3,9 @@ import Projects from "../../projects.json"
 
 
 function Project() {
+  return(
   Projects.map((project)=>{
-    return(<div className="card" style={{ width: "20rem" }}>
+    return(<div className="card border-secondary mb-3" style={{ width: "20rem" }}>
     <img className="card-img-top" src={project.image} alt="Project" />
     <div className="card-body">
       <h5 className="card-title">{project.name}</h5>
@@ -27,29 +28,8 @@ function Project() {
   </div>) 
   
   
-  }
-  )
+}
+  ))
 }
 
 export default Project;
-{/* <div className="card" style={{ width: "20rem" }}>
-        <img className="card-img-top" src={project.image} alt="Project" />
-        <div className="card-body">
-          <h5 className="card-title">{project.name}</h5>
-          <p className="card-text">
-            {project.description}
-          </p>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Technologies Used</li>
-          <li className="list-group-item">{project.technology}</li>
-        </ul>
-        <div className="card-body">
-          <a href={project.github} className="card-link">
-            Link to Repo
-          </a>
-          <a href={project.deploy} className="card-link">
-            Link to deployed App
-          </a>
-        </div>
-      </div> */}
