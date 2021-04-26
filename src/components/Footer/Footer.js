5c953f63cf8f94948a7906136c0f80a284d4e787
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-26 10:02:05 -0700

    attempting to style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer container">
-      <span>Sergio Gomez:</span><br></br>
+    <footer className="container">
+      <span>© 2021 | Made by Sergio Gomez:</span><br></br>
       <a target="blank" className="text-left" href="mailto:sergiogomez100000@gmail.com">Email me!</a>
       <a target="blank" className="text-center" href="https://github.com/sergiogomez100000">Checkout my Github Profile!</a>
       <a target="blank" className="text-right"href="https://www.linkedin.com/in/sergio-gomez-8b3038206/">Checkout my LinkedIn</a>
--- src/components/ProjectInfo/Project.js
+++ src/components/ProjectInfo/Project.js
@@ -1,35 +1,30 @@
 import React from "react";
-import Projects from "../../projects.json"
-
+import Projects from "../../projects.json";
 
 function Project() {
-  return(
-  Projects.map((project)=>{
-    return(<div className="card border-secondary mb-3" style={{ width: "20rem" }}>
-    <img className="card-img-top" src={project.image} alt={project.name} />
-    <div className="card-body">
-      <h5 className="card-title">{project.name}</h5>
-      <p className="card-text">
-        {project.description}
-      </p>
-    </div>
-    <ul className="list-group list-group-flush">
-      <li className="list-group-item">Technologies Used</li>
-      <li className="list-group-item">{project.technology}</li>
-    </ul>
-    <div className="card-body">
-      <a href={project.github} className="card-link">
-        Link to Repo
-      </a>
-      <a href={project.deploy} className="card-link">
-        Link to deployed App
-      </a>
-    </div>
-  </div>) 
-  
-  
-}
-  ))
+  return Projects.map((project) => {
+    return (
+      <div className="card border-secondary mb-3" style={{ width: "20rem" }}>
+        <img className="card-img-top" src={project.image} alt={project.name} />
+        <div className="card-body">
+          <h5 className="card-title">{project.name}</h5>
+          <p className="card-text">{project.description}</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item font-weight-bold">Technologies Used</li>
+          <li className="list-group-item">{project.technology}</li>
+        </ul>
+        <div className="card-body">
+          <a target="blank" href={project.github} className="card-link">
+            Link to Repo
+          </a>
+          <a target="blank" href={project.deploy} className="card-link">
+            Link to deployed App
+          </a>
+        </div>
+      </div>
+    );
+  });
 }
 
 export default Project;
