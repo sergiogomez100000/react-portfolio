0661b64282d0f93376baa50f8e087cac3342d2c7
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-28 18:27:23 -0700

    adding resume comp and link

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,11 @@
+function Resume() {
+    return (
+
+        <div className="container mt-5 col-4">
+       <h3 className="">You can view my resume <a href="https://drive.google.com/file/d/1bOtWCb7b6EiOVYYtSCTE1Wo56kllUjLC/view?usp=sharing" >here!</a></h3>
+       </div>
+    
+    );
+}
+
+export default Resume;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Bio from '../../components/Bio/Bio'
+import Resume from "../../components/Resume/Resume";
+
+
 function Main() {
     return (
+        <>
        <Bio />
+       <Resume />
+       </>
     )
 }
 
